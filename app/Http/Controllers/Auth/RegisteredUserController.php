5907472aaa28f31d55eebceeea34f68a63a1ac86
Silverio <plussilver1@gmail.com>
2022-06-11 19:17:19 -0500

    contraseña segura

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,7 +36,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()->mixedCase()->numbers()],
             'g-recaptcha-response' => 'required|captcha',
         ]);
 
